23834c0e951daf031f7403d5cc9f6f7d8e6ea370
GrahamB <grahamrbeaumont@gmail.com>
2024-01-22 21:18:38 +1000

    First c++ class

--- Source/IsleOfPlunder/Private/Item.cpp
+++ Source/IsleOfPlunder/Private/Item.cpp
@@ -1,10 +1,10 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright of Pixel Piratez
 
-#include "MyActor.h"
-//#include "Items/MyActor.h"
+
+#include "IsleOfPlunder/Public/Item.h"
 
 // Sets default values
-AMyActor::AMyActor()
+AItem::AItem()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -12,14 +12,14 @@ AMyActor::AMyActor()
 }
 
 // Called when the game starts or when spawned
-void AMyActor::BeginPlay()
+void AItem::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
 // Called every frame
-void AMyActor::Tick(float DeltaTime)
+void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
--- Source/IsleOfPlunder/Public/Item.h
+++ Source/IsleOfPlunder/Public/Item.h
@@ -1,26 +1,24 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright of Pixel Piratez
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "MyActor.generated.h"
+#include "Item.generated.h"
 
 UCLASS()
-class ISLEOFPLUNDER_API AMyActor : public AActor
+class ISLEOFPLUNDER_API AItem : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
-	AMyActor();
-
+	AItem();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 };
