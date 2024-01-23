6d73a5216f564e5d5e82801e5588d0dbdafa2e44
GrahamB <grahamrbeaumont@gmail.com>
2024-01-23 22:16:58 +1000

    Debug work

--- Source/IsleOfPlunder/Private/Item.cpp
+++ Source/IsleOfPlunder/Private/Item.cpp
@@ -2,6 +2,11 @@
 
 
 #include "IsleOfPlunder/Public/Item.h"
+#include "DrawDebugHelpers.h"
+#include "IsleOfPlunder/IsleOfPlunder.h"
+
+#define THIRTY 30
+
 
 // Sets default values
 AItem::AItem()
@@ -17,6 +22,16 @@ void AItem::BeginPlay()
 	Super::BeginPlay();
 	//Check Begin play is firing
 	UE_LOG(LogTemp, Warning, TEXT("Begin Play Called!"));
+
+	if(GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, FString("Item OnScreen Message!"));
+	}
+
+	UWorld* World = GetWorld();
+	
+	FVector Location = GetActorLocation();
+	DRAW_SPHERE(Location);
 	
 }
 
@@ -24,6 +39,17 @@ void AItem::BeginPlay()
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	UE_LOG(LogTemp, Warning, TEXT("DeltaTime: %f"), DeltaTime)
+
+	if(GEngine)
+	{
+		FString Name = GetName();
+		FString Message = FString::Printf(TEXT("Item Name: %s"), *Name);
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Cyan, Message);
+
+		UE_LOG(LogTemp, Warning, TEXT("Item Name: %s"), *Name)
+	}
 
 }
 
