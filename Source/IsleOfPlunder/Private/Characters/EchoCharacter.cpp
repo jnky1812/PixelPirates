4935e4ec11c8d506d9c71058a5970abefdaf3569
GrahamB <grahamrbeaumont@gmail.com>
2024-02-13 15:16:32 +1000

    Character Setup

---
+++ Source/IsleOfPlunder/Private/Characters/EchoCharacter.cpp
@@ -0,0 +1,43 @@
+// Copyright of Pixel Piratez
+
+
+#include "Characters/EchoCharacter.h"
+
+// Sets default values
+AEchoCharacter::AEchoCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEchoCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AEchoCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AEchoCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(FName("MoveForward"), this, &AEchoCharacter::MoveForward);
+}
+
+void AEchoCharacter::MoveForward(float Value)
+{
+	if (Controller && (Value != 0.f))
+	{
+		FVector Forward = GetActorForwardVector();
+		AddMovementInput(Forward, Value);
+	}
+}
